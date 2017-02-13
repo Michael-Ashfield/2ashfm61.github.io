bfbe23cd7e290969b5b69afd853acd608e70b091
Michael Ashfield <2ashfm61@solent.ac.uk>
2017-02-13 20:53:04 +0000

    Restructed JS
    Used a validator to restructure and format the JS
    
    JS
    -Restructured for performance and reliability

--- Startpage/mainjs.js
+++ Startpage/mainjs.js
@@ -1,28 +1,31 @@
-function startTime(){
-    var today = new Date();
-    var curr_hour = today.getHours();
-    var curr_minute = today.getMinutes();
-    var curr_second = today.getSeconds();
+function checkTime(i) { // add zero in front of numbers < 10
+    "use strict";
+    if (i < 10) {
+        i = "0" + i
+    }  
+    return i;
+}
+
+function convertTime(curr_hour) {
+    "use strict";
+    if (curr_hour >= 12) {
+        return (curr_hour - 12);
+    }
+}
+
+function startTime() {
+    "use strict";
+    var today = new Date(),
+        curr_hour = today.getHours(),
+        curr_minute = today.getMinutes(),
+        curr_second = today.getSeconds();
     curr_second = checkTime(curr_second);
     curr_minute = checkTime(curr_minute);
     curr_hour = checkTime(curr_hour);
-    var curr_time = convertTime(curr_hour) +":"+ curr_minute +":"+ curr_second;
-    console.log(curr_time);
+    var curr_time = convertTime(curr_hour) + ":" + curr_minute + ":" + curr_second;
     document.getElementById("time").innerHTML = curr_time;  
 }
 
 var myTimer = setInterval(startTime, 500);
 
-function convertTime(curr_hour){
-    if (curr_hour >= 12){
-        return(curr_hour-12);
-    }
-    else {
-        return(curr_hour);
-    }
-}
 
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
